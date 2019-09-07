b5d4420e9d9b3ac4ac0b1d08590e517a722ebec2
Next <wahyupwl02.wp@gmail.com>
2019-09-07 07:43:43 +0700

    Fix templates,views's function,typos,graphs

--- c0tracerstudy/views.py
+++ c0tracerstudy/views.py
@@ -35,11 +35,10 @@ def home_view(request,*args, **kwargs):
     return render(request, "base.html")
 
 def view_graph1(request,*args, **kwargs):
-    f3 = MencariPekerjaan.objects.using('c0tracerstudy').values_list("f3",flat=True)
     listF3=[1,2,3,4,5]
     listMencariPekerjaan = []
     for i in listF3:
-        listMencariPekerjaan.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f3=i).values_list('nim', flat=True))
+        listMencariPekerjaan.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f3=i).values_list('nim', flat=True).distinct())
     for i in range(len(listMencariPekerjaan)):
         listMencariPekerjaan[i] = len(listMencariPekerjaan[i])
     data = [list(i) for i in zip(listMencariPekerjaan,listF3)]
@@ -47,42 +46,32 @@ def view_graph1(request,*args, **kwargs):
     return render(request,"graph1.html",context)
 
 def view_graph2(request,*args, **kwargs):
-    listkoldata = ['f401','f402','f163','f403','f405','f406','f407','f408','f409','f410','f411','f412','f413','f414','f415']
-    listPekerjaan = []
-    for i in listkoldata:
-        listPekerjaan.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(i__in=1).values_list('nim', flat=True))
-    for i in range(len(listPekerjaan)):
-        listPekerjaan[i] = len(listPekerjaan[i])
-    data = [list(i) for i in zip(listPekerjaan, listkoldata)]
+    listkoldata = ['f401', 'f402', 'f163', 'f403', 'f405', 'f406', 'f407', 'f408', 'f409', 'f410', 'f411', 'f412','f413', 'f414', 'f415']
+    A = MencariPekerjaan.objects.filter(f401__contains=1).values_list('nim', flat=True).distinct().count()
+    B = MencariPekerjaan.objects.filter(f402__contains=1).values_list('nim', flat=True).distinct().count()
+    C = MencariPekerjaan.objects.filter(f403__contains=1).values_list('nim', flat=True).distinct().count()
+    D = MencariPekerjaan.objects.filter(f404__contains=1).values_list('nim', flat=True).distinct().count()
+    E = MencariPekerjaan.objects.filter(f405__contains=1).values_list('nim', flat=True).distinct().count()
+    F = MencariPekerjaan.objects.filter(f406__contains=1).values_list('nim', flat=True).distinct().count()
+    G = MencariPekerjaan.objects.filter(f407__contains=1).values_list('nim', flat=True).distinct().count()
+    H = MencariPekerjaan.objects.filter(f408__contains=1).values_list('nim', flat=True).distinct().count()
+    I = MencariPekerjaan.objects.filter(f409__contains=1).values_list('nim', flat=True).distinct().count()
+    J = MencariPekerjaan.objects.filter(f410__contains=1).values_list('nim', flat=True).distinct().count()
+    K = MencariPekerjaan.objects.filter(f411__contains=1).values_list('nim', flat=True).distinct().count()
+    L = MencariPekerjaan.objects.filter(f412__contains=1).values_list('nim', flat=True).distinct().count()
+    M = MencariPekerjaan.objects.filter(f413__contains=1).values_list('nim', flat=True).distinct().count()
+    N = MencariPekerjaan.objects.filter(f414__contains=1).values_list('nim', flat=True).distinct().count()
+    O = MencariPekerjaan.objects.filter(f415__contains=1).values_list('nim', flat=True).distinct().count()
+    listMencariPekerjaan = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O]
+    data = [list(i) for i in zip(listMencariPekerjaan, listkoldata)]
     context = {"data": data}
     return render(request,"graph2.html",context)
-
-# def view_graph2(request,*args, **kwargs):
-# 	data1 = MencariPekerjaan.objects.values('nim','f401','f402','f403','f404','f405','f406','f407','f408','f409','f410','f411','f412','f413','f414','f415')
-# 	A = MencariPekerjaan.objects.filter(f401__contains=1).count()
-# 	B = MencariPekerjaan.objects.filter(f402__contains=1).count()
-# 	C = MencariPekerjaan.objects.filter(f403__contains=1).count()
-# 	D = MencariPekerjaan.objects.filter(f404__contains=1).count()
-# 	E = MencariPekerjaan.objects.filter(f405__contains=1).count()
-# 	F = MencariPekerjaan.objects.filter(f406__contains=1).count()
-# 	G = MencariPekerjaan.objects.filter(f407__contains=1).count()
-# 	H = MencariPekerjaan.objects.filter(f408__contains=1).count()
-# 	I = MencariPekerjaan.objects.filter(f409__contains=1).count()
-# 	J = MencariPekerjaan.objects.filter(f410__contains=1).count()
-# 	K = MencariPekerjaan.objects.filter(f411__contains=1).count()
-# 	L = MencariPekerjaan.objects.filter(f412__contains=1).count()
-# 	M = MencariPekerjaan.objects.filter(f413__contains=1).count()
-# 	N = MencariPekerjaan.objects.filter(f414__contains=1).count()
-# 	O = MencariPekerjaan.objects.filter(f415__contains=1).count()
-# 	P = MencariPekerjaan.objects.filter(f401__contains=0).filter(f402__contains=0).filter(f403__contains=0).filter(f404__contains=0).filter(f405__contains=0).filter(f406__contains=0).filter(f407__contains=0).filter(f408__contains=0).filter(f409__contains=0).filter(f410__contains=0).filter(f411__contains=0).filter(f412__contains=0).filter(f413__contains=0).filter(f414__contains=0).filter(f415__contains=0).count()
-# 	context = {"A":A,"B":B,"C":C,"D":D,"E":E,"F":F,"G":G,"H":H,"I":I,"J":J,"K":K,"L":L,"M":M,"N":N,"O":O,"P":P,"data":data1}
-# 	return render(request,"graph2.html",context)
 
 def view_graph3(request,*args, **kwargs):
     listF5 = [4,5,6,0,1,2,3,7,8]
     listMencariPekerjaan = []
     for i in listF5:
-        listMencariPekerjaan.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f5=i).values_list('nim', flat=True))
+        listMencariPekerjaan.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f5=i).values_list('nim', flat=True).distinct())
     for i in range(len(listMencariPekerjaan)):
         listMencariPekerjaan[i] = len(listMencariPekerjaan[i])
     data = [list(i) for i in zip(listMencariPekerjaan, listF5)]
@@ -94,7 +83,7 @@ def view_graph4(request,*args, **kwargs):
     listMencariPekerjaan = []
     for i in listF6:
         listMencariPekerjaan.append(
-            MencariPekerjaan.objects.using('c0tracerstudy').filter(f6=i).values_list('nim', flat=True))
+            MencariPekerjaan.objects.using('c0tracerstudy').filter(f6=i).values_list('nim', flat=True).distinct())
     for i in range(len(listMencariPekerjaan)):
         listMencariPekerjaan[i] = len(listMencariPekerjaan[i])
     data = [list(i) for i in zip(listMencariPekerjaan, listF6)]
@@ -106,7 +95,7 @@ def view_graph5(request,*args, **kwargs):
     listMencariPekerjaan = []
     for i in listF7:
         listMencariPekerjaan.append(
-            MencariPekerjaan.objects.using('c0tracerstudy').filter(f7=i).values_list('nim', flat=True))
+            MencariPekerjaan.objects.using('c0tracerstudy').filter(f7=i).values_list('nim', flat=True).distinct())
     for i in range(len(listMencariPekerjaan)):
         listMencariPekerjaan[i] = len(listMencariPekerjaan[i])
     data = [list(i) for i in zip(listMencariPekerjaan, listF7)]
@@ -118,7 +107,7 @@ def view_graph6(request,*args, **kwargs):
     listPekerjaanSekarang = []
     for i in listF111:
         listPekerjaanSekarang.append(
-            PekerjaanSekarang.objects.using('c0tracerstudy').filter(f111=i).values_list('nim', flat=True))
+            PekerjaanSekarang.objects.using('c0tracerstudy').filter(f111=i).values_list('nim', flat=True).distinct())
     for i in range(len(listPekerjaanSekarang)):
         listPekerjaanSekarang[i] = len(listPekerjaanSekarang[i])
     data = [list(i) for i in zip(listPekerjaanSekarang, listF111)]
@@ -138,7 +127,7 @@ def view_graph7(request,*args, **kwargs):
     listF12.sort()
     listPekerjaan = []
     for i in listF12:
-        listPekerjaan.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f12=i).values_list('nim', flat=True))
+        listPekerjaan.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f12=i).values_list('nim', flat=True).distinct())
     for i in range(len(listPekerjaan)):
         listPekerjaan[i] = len(listPekerjaan[i])
     data = [list(i) for i in zip(listPekerjaan, listF12)]
@@ -150,7 +139,7 @@ def view_graph8(request,*args, **kwargs):
     listPekerjaanSekarang = []
     for i in listF14:
         listPekerjaanSekarang.append(
-            PekerjaanSekarang.objects.using('c0tracerstudy').filter(f14=i).values_list('nim', flat=True))
+            PekerjaanSekarang.objects.using('c0tracerstudy').filter(f14=i).values_list('nim', flat=True).distinct())
     for i in range(len(listPekerjaanSekarang)):
         listPekerjaanSekarang[i] = len(listPekerjaanSekarang[i])
     data = [list(i) for i in zip(listPekerjaanSekarang, listF14)]
@@ -161,7 +150,7 @@ def view_graph9(request,*args, **kwargs):
     listF15 = [1,2,3,4]
     listPekerjaan = []
     for i in listF15:
-        listPekerjaan.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f15=i).values_list('nim', flat=True))
+        listPekerjaan.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f15=i).values_list('nim', flat=True).distinct())
     for i in range(len(listPekerjaan)):
         listPekerjaan[i] = len(listPekerjaan[i])
     data = [list(i) for i in zip(listPekerjaan, listF15)]
@@ -169,42 +158,30 @@ def view_graph9(request,*args, **kwargs):
     return render(request,"graph9.html",context)
 
 def view_graph10(request,*args, **kwargs):
-    listkoldata = [f161,f162,f163,f164,f165,f166,f167,f168,f169,f1610,f1611,f1612,f1613]
-    listPekerjaan = []
-    for i in listkoldata:
-        listPekerjaan.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(i=1).values_list('nim', flat=True))
-    for i in range(len(listPekerjaan)):
-        listPekerjaan[i] = len(listPekerjaan[i])
-    data = [list(i) for i in zip(listPekerjaan, listkoldata)]
+    listkoldata = ['f161','f162','f163','f164','f165','f166','f167','f168','f169','f1610','f1611','f1612','f1613']
+    A = PekerjaanSekarang.objects.filter(f161__contains=1).values_list('nim', flat=True).distinct().count()
+    B = PekerjaanSekarang.objects.filter(f162__contains=1).values_list('nim', flat=True).distinct().count()
+    C = PekerjaanSekarang.objects.filter(f163__contains=1).values_list('nim', flat=True).distinct().count()
+    D = PekerjaanSekarang.objects.filter(f164__contains=1).values_list('nim', flat=True).distinct().count()
+    E = PekerjaanSekarang.objects.filter(f165__contains=1).values_list('nim', flat=True).distinct().count()
+    F = PekerjaanSekarang.objects.filter(f166__contains=1).values_list('nim', flat=True).distinct().count()
+    G = PekerjaanSekarang.objects.filter(f167__contains=1).values_list('nim', flat=True).distinct().count()
+    H = PekerjaanSekarang.objects.filter(f168__contains=1).values_list('nim', flat=True).distinct().count()
+    I = PekerjaanSekarang.objects.filter(f169__contains=1).values_list('nim', flat=True).distinct().count()
+    J = PekerjaanSekarang.objects.filter(f1610__contains=1).values_list('nim', flat=True).distinct().count()
+    K = PekerjaanSekarang.objects.filter(f1611__contains=1).values_list('nim', flat=True).distinct().count()
+    L = PekerjaanSekarang.objects.filter(f1612__contains=1).values_list('nim', flat=True).distinct().count()
+    M = PekerjaanSekarang.objects.filter(f1613__contains=1).values_list('nim', flat=True).distinct().count()
+    listMencariPekerjaan = [A,B,C,D,E,F,G,H,I,J,K,L,M]
+    data = [list(i) for i in zip(listMencariPekerjaan, listkoldata)]
     context = {"data": data}
     return render(request,"graph10.html",context)
-
-# def view_graph10(request,*args, **kwargs):
-# 	data1 = PekerjaanSekarang.objects.values('nim','f161','f162','f163','f164','f165','f166','f167','f168','f169','f1610','f1611','f1612','f1613','f1614')
-# 	# data2 =
-# 	A = PekerjaanSekarang.objects.filter(f161__contains=1).count()
-# 	B = PekerjaanSekarang.objects.filter(f162__contains=1).count()
-# 	C = PekerjaanSekarang.objects.filter(f163__contains=1).count()
-# 	D = PekerjaanSekarang.objects.filter(f164__contains=1).count()
-# 	E = PekerjaanSekarang.objects.filter(f165__contains=1).count()
-# 	F = PekerjaanSekarang.objects.filter(f166__contains=1).count()
-# 	G = PekerjaanSekarang.objects.filter(f167__contains=1).count()
-# 	H = PekerjaanSekarang.objects.filter(f168__contains=1).count()
-# 	I = PekerjaanSekarang.objects.filter(f169__contains=1).count()
-# 	J = PekerjaanSekarang.objects.filter(f1610__contains=1).count()
-# 	K = PekerjaanSekarang.objects.filter(f1611__contains=1).count()
-# 	L = PekerjaanSekarang.objects.filter(f1612__contains=1).count()
-# 	M = PekerjaanSekarang.objects.filter(f1613__contains=1).count()
-# 	N = PekerjaanSekarang.objects.filter(f161__contains=0,f162__contains=0,f163__contains=0,f164__contains=0,f165__contains=0,f166__contains=0,f167__contains=0,f168__contains=0,f169__contains=0,f1610__contains=0,f1611__contains=0,f1612__contains=0,f1613__contains=0,).count()
-
-# 	context = {"A":A,"B":B,"C":C,"D":D,"E":E,"F":F,"G":G,"H":H,"I":I,"J":J,"K":K,"L":L,"M":M,"N":N, "data":data1}
-# 	return render(request,"graph10.html",context)
 
 def view_graph11(request,*args, **kwargs):
     listF13=[1,2,3,4,5,6]
     listPekerjaan = []
     for i in listF13:
-        listPekerjaan.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f13=i).values_list('nim', flat=True))
+        listPekerjaan.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f13=i).values_list('nim', flat=True).distinct())
     for i in range(len(listPekerjaan)):
         listPekerjaan[i] = len(listPekerjaan[i])
     data = [list(i) for i in zip(listPekerjaan,listF13)]
@@ -213,11 +190,41 @@ def view_graph11(request,*args, **kwargs):
 
 def fill(request, filter, graph):
     listData=[]
-    print(graph)
     listProfil = []
     if request.method == 'POST':
         if graph == 1:
             listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f3=filter).values_list('nim', flat=True))
+        if graph == 2:
+            if filter=="f401":
+                listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f401=1).values_list('nim', flat=True))
+            if filter=="f402":
+                listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f402=1).values_list('nim', flat=True))
+            if filter=="f403":
+                listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f403=1).values_list('nim', flat=True))
+            if filter=="f404":
+                listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f404=1).values_list('nim', flat=True))
+            if filter=="f405":
+                listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f405=1).values_list('nim', flat=True))
+            if filter=="f406":
+                listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f406=1).values_list('nim', flat=True))
+            if filter=="f407":
+                listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f407=1).values_list('nim', flat=True))
+            if filter=="f408":
+                listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f408=1).values_list('nim', flat=True))
+            if filter=="f409":
+                listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f409=1).values_list('nim', flat=True))
+            if filter=="f410":
+                listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f410=1).values_list('nim', flat=True))
+            if filter=="f411":
+                listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f411=1).values_list('nim', flat=True))
+            if filter=="f412":
+                listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f412=1).values_list('nim', flat=True))
+            if filter=="f413":
+                listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f413=1).values_list('nim', flat=True))
+            if filter=="f414":
+                listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f414=1).values_list('nim', flat=True))
+            if filter=="f415":
+                listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f415=1).values_list('nim', flat=True))
         if graph == 3:
             listData.append(MencariPekerjaan.objects.using('c0tracerstudy').filter(f5=filter).values_list('nim', flat=True))
         if graph == 4:
@@ -232,6 +239,33 @@ def fill(request, filter, graph):
             listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f14=filter).values_list('nim', flat=True))
         if graph == 9:
             listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f15=filter).values_list('nim', flat=True))
+        if graph == 10:
+            if filter=="f161":
+                listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f161=1).values_list('nim', flat=True))
+            if filter=="f162":
+                listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f162=1).values_list('nim', flat=True))
+            if filter=="f163":
+                listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f163=1).values_list('nim', flat=True))
+            if filter=="f164":
+                listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f164=1).values_list('nim', flat=True))
+            if filter=="f165":
+                listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f165=1).values_list('nim', flat=True))
+            if filter=="f166":
+                listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f166=1).values_list('nim', flat=True))
+            if filter=="f167":
+                listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f167=1).values_list('nim', flat=True))
+            if filter=="f168":
+                listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f168=1).values_list('nim', flat=True))
+            if filter=="f169":
+                listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f169=1).values_list('nim', flat=True))
+            if filter=="f1610":
+                listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f1610=1).values_list('nim', flat=True))
+            if filter=="f1611":
+                listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f1611=1).values_list('nim', flat=True))
+            if filter=="f1612":
+                listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f1612=1).values_list('nim', flat=True))
+            if filter=="f1613":
+                listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f1613=1).values_list('nim', flat=True))
         if graph == 11:
             listData.append(PekerjaanSekarang.objects.using('c0tracerstudy').filter(f13=filter).values_list('nim', flat=True))
         for i in listData:
